04d1a9d04e2d91d11bf10f1d76498dcea0e7635f
Bracha Rabinovich <Bracha.Rabinovich@cellebrite.com>
2022-10-02 09:37:50 +0300

    Add project files.

---
+++ CrashWithListView/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace CrashWithListView
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            DispatcherUnhandledException += OnUnhandledException;
+        }
+
+        private void OnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            string str = e.Exception.ToString();
+            if (Debugger.IsAttached)
+            {
+                Debugger.Break();
+            }
+
+            MessageBox.Show("Error " + str);
+        }
+    }
+}
---
+++ CrashWithListView/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CrashWithListView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MyList.Items.Add("111");
+            MyList.Items.Add("222");
+            MyList.Items.Add("333");
+            MyList.Items.Add("444");
+            MyList.Items.Add("555");
+            MyList.Items.Add("666");
+            MyList.Items.Add("777");
+        }
+
+        private void AddItemBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MyList.Items.Add("888");
+        }
+    }
+}
